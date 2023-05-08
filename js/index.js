401af0abd74880d4ca88ba6885d4019ed2db2763
Richard Kužela <richard@kanshen.click>
2023-05-08 13:26:10 +0200

    Added Theme SetUp script

--- js/index.js
+++ js/index.js
@@ -7,3 +7,17 @@ function updateTime() {
     document.querySelector('#date').textContent = dateString;
     setInterval(updateTime, 1);
 }
+
+function setUpTheme() {
+    const desktop = document.getElementById('desktop');
+    const themeCookie = document.cookie.split('; ')
+       .find(cookie => cookie.startsWith('theme='));
+    if (themeCookie) {
+       const theme = themeCookie.split('=')[1];
+       desktop.setAttribute('theme', theme);
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        desktop.setAttribute('theme', "dark");
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        desktop.setAttribute('theme', "light");
+    }
+}
