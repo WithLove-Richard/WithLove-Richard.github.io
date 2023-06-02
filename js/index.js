ddb8f896670d592dc55ebd964686d0f63a3cc84f
Richard Kužela <richard@kanshen.click>
2023-06-02 18:00:26 +0200

    Added a test window (WIP)

--- js/index.js
+++ js/index.js
@@ -45,3 +45,68 @@ function setUpTheme() {
     return cookieValue ? cookieValue.pop() : '';
   }
   
+  const windowElement = document.getElementById('myWindow');
+const resizeHandles = [
+  document.getElementById('resize-handle-top-left'),
+  document.getElementById('resize-handle-top-right'),
+  document.getElementById('resize-handle-bottom-left'),
+  document.getElementById('resize-handle-bottom-right')
+];
+let isResizing = false;
+let startX;
+let startY;
+let startWidth;
+let startHeight;
+
+resizeHandles.forEach(handle => {
+  handle.addEventListener('mousedown', startResize);
+});
+
+function startResize(e) {
+  e.preventDefault();
+  isResizing = true;
+  startX = e.clientX;
+  startY = e.clientY;
+  startWidth = parseInt(document.defaultView.getComputedStyle(windowElement).width, 10);
+  startHeight = parseInt(document.defaultView.getComputedStyle(windowElement).height, 10);
+  window.addEventListener('mousemove', resize);
+  window.addEventListener('mouseup', stopResize);
+}
+
+function resize(e) {
+  if (!isResizing) return;
+
+  const deltaX = e.clientX - startX;
+  const deltaY = e.clientY - startY;
+
+  const corner = e.target.classList[1];
+
+  switch (corner) {
+    case 'top-left':
+      windowElement.style.width = `${startWidth - deltaX}px`;
+      windowElement.style.height = `${startHeight - deltaY}px`;
+      windowElement.style.left = `${startX + deltaX}px`;
+      windowElement.style.top = `${startY + deltaY}px`;
+      break;
+    case 'top-right':
+      windowElement.style.width = `${startWidth + deltaX}px`;
+      windowElement.style.height = `${startHeight - deltaY}px`;
+      windowElement.style.top = `${startY + deltaY}px`;
+      break;
+    case 'bottom-left':
+      windowElement.style.width = `${startWidth - deltaX}px`;
+      windowElement.style.height = `${startHeight + deltaY}px`;
+      windowElement.style.left = `${startX + deltaX}px`;
+      break;
+    case 'bottom-right':
+      windowElement.style.width = `${startWidth + deltaX}px`;
+      windowElement.style.height = `${startHeight + deltaY}px`;
+      break;
+  }
+}
+
+function stopResize() {
+  isResizing = false;
+  window.removeEventListener('mousemove', resize);
+  window.removeEventListener('mouseup', stopResize);
+}
